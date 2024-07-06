49b9240660890656147c4434dcac9494440d8185
TornikeGorashvili <tokogorashvili@gmail.com>
2024-07-06 10:48:22 +0400

    second commit

---
+++ lesson04/classwork/classwork00.py
@@ -0,0 +1,18 @@
+print(age)
+age=12
+#print-ი წერია age-ს წინ 
+
+name= tornike
+print(name)
+#name-ს ბრჭყალები არ უწერია
+
+print(something)
+#something-ს ბრჭყალები არ უწერია
+
+Number = 12
+print(number)
+#number პატარა ასოთი იწყება
+
+thing = "phone"
+print("thing")
+#thing-ი ბრწყალეში წერია 
