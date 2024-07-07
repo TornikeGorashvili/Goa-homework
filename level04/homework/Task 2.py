f91e0a497f8e5bec73579ee3e9eb7d56f82f5f94
TornikeGorashvili <tokogorashvili@gmail.com>
2024-07-07 08:44:46 +0400

    second commit

---
+++ level04/homework/Task 2.py
@@ -0,0 +1,19 @@
+Something = "something"
+print("Something")
+#something_01-ი ბრწყალებში წერია 
+
+print(year)
+year=2024
+#print-ი წერია age-ს წინ 
+
+sound = music
+print(sound)
+#sound-ს ბრჭყალები არ უწერია
+
+print(something)
+#something-ს ბრჭყალები არ უწერია
+
+Art = Mona_lisa
+print(Art)
+#number პატარა ასოთი იწყება
+
---
+++ level04/homework/Task 3.py
@@ -0,0 +1,2 @@
+year_and_month=("2024-th july")
+print(year_and_month)
