cc48d4dea4bb341048f66380744d87df9fd6ced5
TornikeGorashvili <tokogorashvili@gmail.com>
2024-11-21 23:01:43 +0400

    second commit

---
+++ level45/classwork/Task 1.py
@@ -0,0 +1,33 @@
+# 1
+
+words = ["ვაშლი", "მსხალი", "ატამი", "ბალი", "ტყემალი"]
+
+for word in words:
+    print(word)
+
+
+# 2
+
+numbers = [10, 20, 30, 40, 50]
+
+for number in numbers:
+    print(number)
+
+# 3
+
+numbers = [2, 3, 4, 5, 6]
+
+for number in numbers:
+    print(number * number)
+
+
+
+# BONUS ONE
+
+numbers = [1, 2, 3, 4, 5]
+
+doubled_numbers = []
+for number in numbers:
+    doubled_numbers.append(number * 2)
+
+print(doubled_numbers)
