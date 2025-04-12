268ed35a14801044d5937571934ce01377bc3542
TornikeGorashvili <tokogorashvili@gmail.com>
2025-04-12 16:38:02 +0400

    second commit

---
+++ level86/classwork/script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function counter1() {
+    count = 0;
+    document.getElementById('count').textContent = count;
+}
+
+function counter2() {
+    count = 50;
+    document.getElementById('count').textContent = count;
+}
